edd7237ff051034d30ea7c7dc62ca346c0c0828a
ccasado <ccasado@corp.globo.com>
2016-06-15 20:24:35 -0300

    more metrics

--- main.py
+++ main.py
@@ -17,12 +17,12 @@ args = parser.parse_args()
 
 config = Config()
 env = args.env
+project = config.ConfigSectionMap(env)['project'] 
 eshost = config.ConfigSectionMap(env)['eshost']
 esport = config.ConfigSectionMap(env)['esport']
 esclustername = config.ConfigSectionMap(env)['esclustername']
 statsHost = config.ConfigSectionMap("Statsd")['statshost']
 statsPort = config.ConfigSectionMap("Statsd")['statsport']
-statsProject = config.ConfigSectionMap("Statsd")['statsproject']
 stats = statsd.StatsClient(statsHost, statsPort)
 
 def metricsByNodes():
@@ -37,23 +37,33 @@ def metricsByNodes():
 		metrics = {}
 		for node in es_stats["nodes"].values():
 			node_name = node["name"]
-			metrics[node_name + '.os.cpu_percent'] = node["os"]["cpu_percent"]
-			metrics[node_name + '.os.load_average'] = node["os"]["load_average"]
-			metrics[node_name + '.os.mem.free_percent'] = node["os"]["mem"]["free_percent"]
-			metrics[node_name + '.os.mem.used_percent'] = node["os"]["mem"]["used_percent"]
-			metrics[node_name + '.os.swap.free_in_bytes'] = node["os"]["swap"]["free_in_bytes"]
-			metrics[node_name + '.os.swap.used_in_bytes'] = node["os"]["swap"]["used_in_bytes"]
-			metrics[node_name + '.jvm.mem.heap_used_percent'] = node["jvm"]["mem"]["heap_used_percent"]
+			first_key = project + '.' + env + '.' + esclustername + '.' + node_name 
+			metrics[first_key + '.os.cpu_percent'] = node["os"]["cpu_percent"]
+			metrics[first_key + '.os.load_average'] = node["os"]["load_average"]
+			metrics[first_key + '.os.mem.free_percent'] = node["os"]["mem"]["free_percent"]
+			metrics[first_key + '.os.mem.used_percent'] = node["os"]["mem"]["used_percent"]
+			#metrics[first_key + '.os.swap.free_in_bytes'] = node["os"]["swap"]["free_in_bytes"]
+			metrics[first_key + '.os.swap.used_in_bytes'] = node["os"]["swap"]["used_in_bytes"]
+			metrics[first_key + '.jvm.mem.heap_used_percent'] = node["jvm"]["mem"]["heap_used_percent"]
+			metrics[first_key + '.indices.indexing.index_current'] = node["indices"]["indexing"]["index_current"]
+			metrics[first_key + '.indices.search.query_current'] = node["indices"]["search"]["query_current"]
+			metrics[first_key + '.indices.docs.count'] = node["indices"]["docs"]["count"]
+			#metrics[first_key + '.indices.docs.deleted'] = node["indices"]["docs"]["deleted"]
+			metrics[first_key + '.indices.segments.count'] = node["indices"]["segments"]["count"]
+			metrics[first_key + '.fs.total.total_in_bytes'] = node["fs"]["total"]["total_in_bytes"]
+			metrics[first_key + '.fs.total.free_in_bytes'] = node["fs"]["total"]["free_in_bytes"]
+			metrics[first_key + '.jvm.gc.collectors.young.collection_time_in_millis'] = node["jvm"]["gc"]["collectors"]["young"]["collection_time_in_millis"]
+			metrics[first_key + '.jvm.gc.collectors.old.collection_time_in_millis'] = node["jvm"]["gc"]["collectors"]["old"]["collection_time_in_millis"]
 		return metrics
 	else:
 		print "Cluster data not available. Check esclustername in your properties file."
 		sys.exit(1)
 
 def sendToStatsd(key, value):
-	#print "%s Sending to statsd - %s:%s" % (strftime("%d %b %Y %H:%M:%S", gmtime()), key, value)
 	stats.incr(key, value)
+	#print "%s Sending to statsd - %s:%s" % (strftime("%d %b %Y %H:%M:%S", gmtime()), key, value)
 
 if __name__ == '__main__':
 	for key, value in metricsByNodes().iteritems():
-		print key, value
+		sendToStatsd(key, value)
 
