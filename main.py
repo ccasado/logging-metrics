8ddec8f9d40de51117a78cac6bea525fc5c124cb
ccasado <ccasado@corp.globo.com>
2016-06-27 15:22:37 -0300

    refactor logging envs

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 
-from Config import Config
 import requests
 import statsd
 import json
@@ -9,37 +8,22 @@ import sys
 import sched, time
 from time import gmtime, strftime
 import os
-
-parser = argparse.ArgumentParser(description='Logging statistics using Statsd, Graphite e Graphana.')
-parser.add_argument('--env', type=str, choices=['devqa', 'prod'], required=True,
-                    help='Run the collect data on this environment')
-args = parser.parse_args()
-
-config = Config()
-env = args.env
-eshost = config.ConfigSectionMap(env)['eshost']
-esport = config.ConfigSectionMap(env)['esport']
-esclustername = config.ConfigSectionMap(env)['esclustername']
-esnodesname = config.ConfigSectionMap(env)['esnodesname']
-project = config.ConfigSectionMap("Statsd")['project'] 
-statsHost = config.ConfigSectionMap("Statsd")['statshost']
-statsPort = config.ConfigSectionMap("Statsd")['statsport']
-stats = statsd.StatsClient(statsHost, statsPort)
+import logging
 
 def metricsByNodes():
 	try:
-		url = 'http://' + eshost + ':' + esport + '/_nodes/stats'
+		url = 'http://' + ESHOST + ':' + ESPORT + '/_nodes/stats'
 		r = requests.get(url, timeout=1.000)
 	except requests.exceptions.RequestException as e:
-		print e
+		logging.exception(e)
 		sys.exit(1)
 	es_stats = json.loads(r.text)
-	if esclustername == es_stats["cluster_name"]:
+	if ESCLUSTERNAME == es_stats["cluster_name"]:
 		metrics = {}
 		for node in es_stats["nodes"].values():
 			node_name = node["name"]
-			if node_name in esnodesname:
-				first_key = project + '.' + env + '.' + esclustername + '.' + node_name 
+			if node_name in ESNODESNAME:
+				first_key = PROJECT + '.' + ENV + '.' + ESCLUSTERNAME + '.' + node_name 
 				metrics[first_key + '.os.cpu_percent'] = node["os"]["cpu_percent"]
 				metrics[first_key + '.os.load_average'] = node["os"]["load_average"]
 				metrics[first_key + '.os.mem.free_percent'] = node["os"]["mem"]["free_percent"]
@@ -58,21 +42,21 @@ def metricsByNodes():
 
 		return metrics
 	else:
-		print "Cluster data not available. Check esclustername in your properties file."
+		logging.error("Cluster data not available. Check ESCLUSTERNAME in your variables.")
 		sys.exit(1)
 
 def metricsByCluster():
 	try:
-		url = 'http://' + eshost + ':' + esport + '/_cluster/health'
+		url = 'http://' + ESHOST + ':' + ESPORT + '/_cluster/health'
 		payload = {'level': 'shards'}
 		r = requests.get(url, params=payload, timeout=1.000)
 	except requests.exceptions.RequestException as e:
-		print e
+		logging.exception(e)
 		sys.exit(1)
 	es_stats = json.loads(r.text)
-	if esclustername == es_stats["cluster_name"]:
+	if ESCLUSTERNAME == es_stats["cluster_name"]:
 		metrics = {}
-		first_key = project + '.' + env + '.' + esclustername
+		first_key = PROJECT + '.' + ENV + '.' + ESCLUSTERNAME
 		if es_stats["status"] == 'green':
 			status = 0
 		elif es_stats["status"] == 'yellow':
@@ -87,14 +71,37 @@ def metricsByCluster():
 		metrics[first_key + '.unassigned_shards'] = es_stats["unassigned_shards"]
 		return metrics
 	else:
-		print "Cluster data not available. Check esclustername in your properties file."
+		logging.error("Cluster data not available. Check ESCLUSTERNAME in your variables.")
 		sys.exit(1)
 
 def sendToStatsd(key, value):
 	stats.incr(key, value)
-	#print "%s Sending to statsd - %s:%s" % (strftime("%d %b %Y %H:%M:%S", gmtime()), key, value)
+	#logging.debug("%s Sending to statsd - %s:%s") % (strftime("%d %b %Y %H:%M:%S", gmtime()), key, value)
 
 if __name__ == '__main__':
+	logging.basicConfig(level=logging.INFO, format='%(levelname)s %(message)s')
+	parser = argparse.ArgumentParser(description='Logging statistics using Statsd, Graphite e Graphana.')
+	parser.add_argument('--env', type=str, choices=['devqa', 'prod'], required=True,
+                    help='Run the collect data on this environment')
+	args = parser.parse_args()
+
+	ENV = args.env
+	if ENV == 'prod':
+		ESHOST = os.environ['ESHOST_PROD']
+		ESPORT = os.environ['ESPORT_PROD']
+		ESCLUSTERNAME = os.environ['ESCLUSTERNAME_PROD']
+		ESNODESNAME = os.environ['ESNODESNAME_PROD']
+	else:
+		ESHOST = os.environ['ESHOST_DEVQA']
+		ESPORT = os.environ['ESPORT_DEVQA']
+		ESCLUSTERNAME = os.environ['ESCLUSTERNAME_DEVQA']
+		ESNODESNAME = os.environ['ESNODESNAME_DEVQA']
+
+	PROJECT = os.environ['PROJECT']
+	STATSD_HOST = os.environ['STATSD_HOST']
+	STATSD_PORT = os.environ['STATSD_PORT']
+	STATSD = statsd.StatsClient(STATSD_HOST, STATSD_PORT)
+	
 	s = sched.scheduler(time.time, time.sleep)
 	def goahed(sc):
 		for key, value in metricsByNodes().iteritems():
@@ -105,4 +112,3 @@ if __name__ == '__main__':
 		
 	s.enter(10, 1, goahed, (s,))
 	s.run()
-
